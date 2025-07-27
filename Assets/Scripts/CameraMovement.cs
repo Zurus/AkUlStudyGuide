039df0e6f968e0e41c68ddf3fc8ecc76fe027af1
WIN-34GA4MSGIH7\Artur <zulkund@mail.ru>
2025-07-27 21:18:21 +0500

    Добавлены границы камеры

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,6 +8,9 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
     public float smoothing;
 
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
 
     void Start()
     {
@@ -19,7 +22,15 @@ public class CameraMovement : MonoBehaviour
     {
         if (transform.position != target.position)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            Vector3 targetPosition = new Vector3(
+                target.position.x, 
+                target.position.y, 
+                transform.position.z
+            );
+            targetPosition.x = Mathf.Clamp( targetPosition.x, minPosition.x, maxPosition.x );
+            targetPosition.y = Mathf.Clamp( targetPosition.y, minPosition.y, maxPosition.y );
+            
+            
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
 
         }
